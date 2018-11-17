5a96131addff3796e0a2c636745ffccd929ba72e
Marcu marian <rcmarcu@gmail.com>
2018-11-17 23:56:39 +0200

    initial solution

---
+++ RideMatch/RideMatch/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RideMatch.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
