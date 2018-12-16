dc4aa6e3c3476d05502945048d4bd55aee9b6d5b
Marcu marian <rcmarcu@gmail.com>
2018-12-16 23:16:26 +0200

    add azure active directory

---
+++ Source/RideMatch/Controllers/SecurityController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RideMatch.Controllers
+{
+	public class SecurityController : Controller
+	{
+		public IActionResult Login()
+		{
+			return Challenge(new AuthenticationProperties { RedirectUri = "/" });
+		}
+
+		public async Task Logout()
+		{
+			await HttpContext.SignOutAsync(
+				CookieAuthenticationDefaults.AuthenticationScheme);
+			await HttpContext.SignOutAsync(
+				OpenIdConnectDefaults.AuthenticationScheme);
+		}
+	}
+}
--- Source/RideMatch/Startup.cs
+++ Source/RideMatch/Startup.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using RideMatch.Models;
 
 namespace RideMatch
 {
@@ -29,7 +33,26 @@ namespace RideMatch
 				options.CheckConsentNeeded = context => true;
 				options.MinimumSameSitePolicy = SameSiteMode.None;
 			});
+			var azureSettingsSection = Configuration.GetSection("AzureSettings");
+			var azureSettings = azureSettingsSection.Get<AzureSettings>();
 
+			services.AddAuthentication(options =>
+			{
+				options.DefaultAuthenticateScheme =
+					CookieAuthenticationDefaults.AuthenticationScheme;
+				options.DefaultSignInScheme =
+					CookieAuthenticationDefaults.AuthenticationScheme;
+				options.DefaultChallengeScheme =
+					OpenIdConnectDefaults.AuthenticationScheme;
+			}).AddOpenIdConnect(options =>
+			{
+				options.Authority = "https://login.microsoftonline.com/" +
+				                    azureSettings.Tenant;
+				options.ClientId = azureSettings.ClientId;
+				options.ResponseType = OpenIdConnectResponseType.IdToken;
+				options.CallbackPath = "/security/signin-callback";
+				options.SignedOutRedirectUri = azureSettings.RootUrl;
+			}).AddCookie();
 
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 		}
